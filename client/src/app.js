54aac681dfa35644e4d993a9ea6f3155e528ac50
hannahgreen1 <hannahgreen94@icloud.com>
2018-07-06 13:22:41 +0100

    drone list view

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,5 @@
 document.addEventListner('DOMConetenLoaded', ()=> {
   console.log("hello");
 
-  const drones = new Drones('https://api.dronestre.am/data')
-  
+const drones = new Drones;
 })
--- client/src/models/drones.js
+++ client/src/models/drones.js
@@ -1,11 +1,18 @@
 const Request = require('../helpers/request.js');
 const PubSub = require('../helpers/pub_sub.js');
 
-const Drones = function (url) {
-  this.url = url;
+const Drones = function () {
+  this.dronesData = [];
 };
 
+Drones.prototype.getData = function () {
+  const request = new Request('https://api.dronestre.am/data')
+  request.get((data) =>{
+    PubSub.publish('Drones:drones-ready', data);
+  })
 
+
+};
 
 
 
