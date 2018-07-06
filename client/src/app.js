aa78a70a3640e297920bdd473cc683f0773b056a
hannahgreen1 <hannahgreen94@icloud.com>
2018-07-06 13:07:39 +0100

    app content loaded

---
+++ client/src/app.js
@@ -0,0 +1,6 @@
+document.addEventListner('DOMConetenLoaded', ()=> {
+  console.log("hello");
+
+  const drones = new Drones('https://api.dronestre.am/data')
+  
+})
---
+++ client/src/models/drones.js
@@ -0,0 +1,12 @@
+const Request = require('../helpers/request.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+const Drones = function (url) {
+  this.url = url;
+};
+
+
+
+
+
+module.export = Drones;
