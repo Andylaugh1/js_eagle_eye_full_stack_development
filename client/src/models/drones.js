3eeac02b0cb73031ea4bf6b99d51a2aa6ae7f1e4
hannahgreen1 <hannahgreen94@icloud.com>
2018-07-06 14:06:56 +0100

    drones.js fixed

--- client/src/models/drones.js
+++ client/src/models/drones.js
@@ -1,17 +1,18 @@
 const Request = require('../helpers/request.js');
 const PubSub = require('../helpers/pub_sub.js');
 
-const Drones = function () {
+const Drones = function (url) {
+  this.url = url;
   this.dronesData = [];
 };
 
 Drones.prototype.getData = function () {
-  const request = new Request('https://api.dronestre.am/data')
-  request.get((data) =>{
-    PubSub.publish('Drones:drones-ready', data);
-    console.log(data);
-  })
-
+    const request = new Request(this.url);
+    request.get()
+      .then((data) => {
+        PubSub.publish('Drones:data-loaded', games);
+      })
+      .catch(console.error);
 
 };
 
