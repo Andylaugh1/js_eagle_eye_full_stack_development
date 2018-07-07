e87ce11ba11fc6625dce06badb9b9ba4f0c30962
Andy Laughlin <atl11384@gmail.com>
2018-07-07 13:58:34 +0100

    globe_view code tidied

--- client/src/views/globe_view.js
+++ client/src/views/globe_view.js
@@ -9,39 +9,39 @@ const GlobeView = function(container, container2){
 }
 
 GlobeView.prototype.bindEvents = function () {
-PubSub.subscribe('Drones:data-ready', (evt) =>{
-  droneData = evt.detail;
-  this.render(droneData);
-  this.mapRender(droneData);
+  PubSub.subscribe('Drones:data-ready', (evt) =>{
+    droneData = evt.detail;
+    // this.render(droneData);
+    this.mapRender(droneData);
   });
 };
 
 
-GlobeView.prototype.render = function (droneData) {
-  droneData.strike.forEach((drone, index) => {
-    const strike = document.createElement('p');
-    strike.value = index;
-    var lat = drone.lat;
-    var lon = drone.lon;
-  });
+// GlobeView.prototype.render = function (droneData) {
+//   droneData.strike.forEach((drone, index) => {
+//     const strike = document.createElement('p');
+//     strike.value = index;
+//     var lat = drone.lat;
+//     var lon = drone.lon;
+//   });
 
-};
+// };
 
 GlobeView.prototype.mapRender = function (droneData) {
 
-  var mymap = L.map(this.container2).setView([25, 56.4], 4.5);
+  var mymap = L.map(this.container2).setView([16.9, 55.76], 4.5);
   droneData.strike.forEach((drone, index) => {
-  L.marker([drone.lat, drone.lon]).addTo(mymap);
-})
-// var marker = L.marker([25, 56]).addTo(mymap);
+    L.marker([drone.lat, drone.lon]).addTo(mymap);
+  })
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox.emerald',
-  accessToken: 'pk.eyJ1IjoiYXQtbGF1Z2hsaW4iLCJhIjoiY2pqYjhjc2VkM21uaDNxbzR2aGh3Ynh5ciJ9.N4uJndedEpzsbVCwTCNnJw'
-}).addTo(mymap);
-
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution:
+    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.emerald',
+    accessToken: 'pk.eyJ1IjoiYXQtbGF1Z2hsaW4iLCJhIjoiY2pqYjhjc2VkM21uaDNxbzR2aGh3Ynh5ciJ9.N4uJndedEpzsbVCwTCNnJw'
+  })
+  .addTo(mymap);
 };
 
 
